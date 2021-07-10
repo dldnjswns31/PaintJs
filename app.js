4f547e5e2c505954ae2cfba6a8038dc5b5a1967f
dldnjswns31 <dldnjswns32@gmail.com>
2021-07-10 19:46:47 +0900

    2.3 Changing Color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -15,31 +16,31 @@ function stopPainting(){
 }
 
 function startPainting(){
-    paint = true;
+    painting = true;
 }
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
     if(!painting){
-        console.log("creating path in", x, y);
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
-        console.log("creating line in ", x, y);
         ctx.lineTo(x, y);
         ctx.stroke();
     }
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
-
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
